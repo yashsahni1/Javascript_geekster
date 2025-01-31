7df734e98878609fb8092dffaf1290e69e0cd20d
yash sahni <sahniyash96@gmail.com>
2025-01-31 14:42:03 +0530

    array

---
+++ array.js
@@ -0,0 +1,46 @@
+// let arr=[true,false,1,2,null,undefined,"yash","string"];
+// console.log(arr);
+
+// class{
+//     int length;
+//     property
+//     length(){
+//         method
+//     }
+// }
+
+// let arr=[
+//     {name:"test1",score:100},
+//     {name:"test2",score:200},
+//     {name:"test3",score:300},
+//     {name:"test4",score:400},
+//     {name:"test5",score:500}
+// ]
+
+// console.log(arr.length);
+
+// arr.push({name:"test6",score:600});
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.unshift({name:"test6",score:600});
+// console.log(arr);
+// arr.shift({name:"test6",score:600});
+// console.log(arr);
+// arr.reverse();
+// console.log(arr);
+
+// creates a new Array.........................
+
+// let newSlicedArray1=arr.slice(1,4);
+// let newSlicedArray2=arr.slice(1);
+// console.log(newSlicedArray1);
+// console.log(newSlicedArray2);
+// //new array
+//modifies the original array and delete the inbetween elements.........................
+
+// arr.splice(1,3,2,3,4);
+// console.log(arr);
+
+// arr.splice(1,3,88,88,88);
+// console.log(arr);
