f0ddbfc99709d99452f0723a8005bf6ff06b815d
yash sahni <sahniyash96@gmail.com>
2025-02-08 19:01:13 +0530

    now one more commit

---
+++ strings.js
@@ -0,0 +1,43 @@
+// let playerInfo=["anuj","priyansh","suraj"];
+// let playerToString=playerInfo.toString();
+// console.log(playerToString);
+
+// // output : anuj,priyansh,suraj
+
+// let playerJoin=playerInfo.join("-");
+// console.log(playerJoin);
+
+// //output : anuj-priyansh-suraj
+
+
+// let playerInfoString="anuj-priyansh-suraj";
+// let playerSplit=playerInfoString.split("-");
+// console.log(playerSplit);
+
+// //output : [ 'anuj', 'priyansh', 'suraj' ]
+  
+// let ones=[1,2,3,4,5];
+// delete ones[2];
+// console.log(ones);
+// ones[2]="yash";
+// console.log(ones);
+
+
+// let arr=[
+//     {name:"test1",score:100},
+//     {name:"test2",score:150},
+//     {name:"test3",score:300},
+//     {name:"test4",score:200},
+//     {name:"test5",score:250}
+// ]
+// // arr.sort(function(a,b){
+// //     return a.score-b.score;
+// // });
+// arr.sort(function(a,b){
+//     return b.score-a.score;
+// });
+
+// console.log(arr);
+
+// let arr=[100,110,120,200,101,90];
+// console.log(arr.sort());
